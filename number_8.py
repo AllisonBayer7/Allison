57be8762e127548fc366811efe76c3f20e49006b
Allison Bayer <allisonrbayer@students.abtech.edu>
2026-01-22 15:04:31 -0500

    Add files via upload

---
+++ number_8.py
@@ -0,0 +1,9 @@
+#all operations should equal 8
+#addition
+print(4+4)
+#subtraction
+print(16-8)
+#multiplication
+print(2*4)
+#division
+print(64/8)
---
+++ uppernamecase.py
@@ -0,0 +1,5 @@
+
+name = "Hailey Riddle"
+print(name.upper())
+print(name.lower())
+print(name.title())
