b96a7bb4f334058ff6c45cab0a9260254c614714
Allison Bayer <allisonrbayer@students.abtech.edu>
2026-02-01 16:14:49 -0500

    Add files via upload

---
+++ 3-3 My Own List.py
@@ -0,0 +1,11 @@
+cars=['Honda','Toyota','BMW','Audi','Kia']# My own list of cars
+message=f'At the moment, I own a {cars[0]}.'# Honda is at index 0
+print(message)
+message=f'I used to own a {cars[1]}.'# Toyota is at index 1
+print(message)
+message=f'My dream car is a {cars[2]}.'# BMW is at index 2
+print(message)
+message=f'I would settle for a {cars[3]} too.'# Audi is at index 3
+print(message)
+message=f'I will probably end up with a {cars[4]} though.'# Kia is at index 4
+print(message)
