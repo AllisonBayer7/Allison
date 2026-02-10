484e347d21a1fd324e4d17858aa9ed756c1a83f4
Allison Bayer <allisonrbayer@students.abtech.edu>
2026-02-10 07:22:26 -0500

    Add files via upload

---
+++ 4-2 Animals.py
@@ -0,0 +1,4 @@
+animals=['giraffe','elephant','water buffalo']#animal list
+for animal in animals:#for loop
+    print(f"A {animal} would not make a good pet.")#print statement
+print("\nAll of these animals need to be in the wild.")#reason not to keep as pets
