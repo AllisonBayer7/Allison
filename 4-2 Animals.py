f2fdfa3ead5487bb02b72fc2e9344a5a61ca4174
Allison Bayer <allisonrbayer@students.abtech.edu>
2026-02-16 11:46:48 -0500

    Add files via upload

--- 4-2 Animals.py
+++ 4-2 Animals.py
@@ -1,4 +1,7 @@
-animals=['giraffe','elephant','water buffalo']#animal list
-for animal in animals:#for loop
-    print(f"A {animal} would not make a good pet.")#print statement
-print("\nAll of these animals need to be in the wild.")#reason not to keep as pets
+
+animals = ['giraffe', 'elephant', 'water buffalo']  # animal list
+for animal in animals:  # for loop
+    print(f"A {animal} would not make a good pet.")
+    # print statement
+print("\nAll of these animals need to be in the wild.")
+# reason not to keep"as pets"
---
+++ 4-7 Threes.py
@@ -0,0 +1,7 @@
+# list comprehension to create a list of the first 10 multiples of 3
+threes = [3*x for x in range(1, 11)]
+print(threes)
+
+# list comprehension to create a list of the first 10 cubes
+cubes = [value**3 for value in range(1, 11)]
+print(cubes)
